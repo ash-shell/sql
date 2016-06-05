3e934651d1e561deeb2c7fad1a3bb46ee2ff5a53
BrandonRomano <BrandonRRomano@gmail.com>
2016-06-05 16:05:50 -0400

    Setting defaults

--- lib/drivers/mysql.sh
+++ lib/drivers/mysql.sh
@@ -12,17 +12,32 @@ Sql_MYSQL_CONFIG_FILE="/tmp/ash_sql_mysql_config.cnf"
 Sql__execute() {
     "$SQL_MYSQL_PATH" \
         --defaults-extra-file="$Sql_MYSQL_CONFIG_FILE" \
-        --database="$SQL_DATABASE_NAME" \
+        --database="$SQL_MYSQL_DATABASE_NAME" \
         --silent \
         --skip-column-names \
         --batch \
-        --execute="$1"
+        --execute="$1" \
+        2>&1
 }
 
 #################################################
 # Generates the MySQL config file to be used
 #################################################
 Sql_driver_open() {
+    # Setting defaults, if not set
+    if [[ "$SQL_MYSQL_PATH" = "" ]]; then
+        SQL_MYSQL_PATH="mysql"
+    fi
+    if [[ "$SQL_MYSQL_USER" = "" ]]; then
+        SQL_MYSQL_USER="root"
+    fi
+    if [[ "$SQL_MYSQL_HOST" = "" ]]; then
+        SQL_MYSQL_HOST="localhost"
+    fi
+    if [[ "$SQL_MYSQL_PORT" = "" ]]; then
+        SQL_MYSQL_PORT="3306"
+    fi
+
     # Deleting old mysql config file if it existed
     if [[ -e "$Sql_MYSQL_CONFIG_FILE" ]]; then
         rm "$Sql_MYSQL_CONFIG_FILE"
@@ -33,10 +48,10 @@ Sql_driver_open() {
     chmod 600 "$Sql_MYSQL_CONFIG_FILE"
 
     echo "[client]" >> "$Sql_MYSQL_CONFIG_FILE"
-    echo "user = $SQL_USER" >> "$Sql_MYSQL_CONFIG_FILE"
-    echo "password = $SQL_PASSWORD" >> "$Sql_MYSQL_CONFIG_FILE"
-    echo "host = $SQL_HOST" >> "$Sql_MYSQL_CONFIG_FILE"
-    echo "port = $SQL_PORT" >> "$Sql_MYSQL_CONFIG_FILE"
+    echo "user = $SQL_MYSQL_USER" >> "$Sql_MYSQL_CONFIG_FILE"
+    echo "password = $SQL_MYSQL_PASSWORD" >> "$Sql_MYSQL_CONFIG_FILE"
+    echo "host = $SQL_MYSQL_HOST" >> "$Sql_MYSQL_CONFIG_FILE"
+    echo "port = $SQL_MYSQL_PORT" >> "$Sql_MYSQL_CONFIG_FILE"
 }
 
 #################################################
--- lib/drivers/postgres.sh
+++ lib/drivers/postgres.sh
@@ -8,9 +8,10 @@
 #################################################
 Sql__execute() {
     output=$("$SQL_POSTGRES_PATH" \
-        --username "$SQL_USER" \
-        --dbname "$SQL_DATABASE_NAME" \
-        --host "$SQL_HOST" \
+        --username "$SQL_POSTGRES_USER" \
+        --dbname "$SQL_POSTGRES_DATABASE_NAME" \
+        --host "$SQL_POSTGRES_HOST" \
+        --port "$SQL_POSTGRES_PORT" \
         --command "$1" \
         --no-align \
         --field-separator="	" \
@@ -25,7 +26,19 @@ Sql__execute() {
 # Do nothing, no driver specific setup
 #################################################
 Sql_driver_open() {
-    :
+    # Setting defaults, if not set
+    if [[ "$SQL_POSTGRES_PATH" = "" ]]; then
+        SQL_POSTGRES_PATH="psql"
+    fi
+    if [[ "$SQL_POSTGRES_USER" = "" ]]; then
+        SQL_POSTGRES_USER="postgres"
+    fi
+    if [[ "$SQL_POSTGRES_HOST" = "" ]]; then
+        SQL_POSTGRES_HOST="localhost"
+    fi
+    if [[ "$SQL_POSTGRES_PORT" = "" ]]; then
+        SQL_POSTGRES_PORT="5432"
+    fi
 }
 
 #################################################
